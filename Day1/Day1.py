c9b505ece801003964629c4b37a11c48cd18a96c
wengyan <yan.weng@outlook.com>
2018-09-09 16:54:44 +0800

    day1 done

---
+++ Day1/Day1.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+
+
+# Import dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
+#print X
+#print Y
+
+
+# Handling the missing data
+# http://scikit-learn.org/stable/modules/preprocessing.html#imputation-of-missing-values
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+#print X
+
+
+# Encoding categorical data
+# https://machinelearningmastery.com/how-to-one-hot-encode-sequence-data-in-python/
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+#print X
+
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+#print X
+#print Y
+
+
+# Splitting the datasets into training sets and test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+print X_train
+print X_test
+print Y_train
+print Y_test
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
+print X_train
+print X_test
